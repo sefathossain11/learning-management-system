34094dc965186f92fc47f614a731adf94a4af5fd
Sadman Sakib <sadmansakib452@gmail.com>
2022-10-22 00:11:32 +0600

    added user login system in login page

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// login page start
+
+// saved user mail & password
+
+// user 1 info
+const email = 'sifathossainsiam191090@gmail.com';
+const password = 123456;
+
+
+
+const loginBtn = document.getElementById("loginBtn").addEventListener('click',function(){
+
+const getEmail = document.getElementById('inputEmail3').value;
+let getPassword = document.getElementById('inputPassword3').value;
+getPassword = parseInt(getPassword);
+
+if(getEmail === email && getPassword === password){
+    
+    window.location.href = "dashboard/dashboard.html";
+    console.log('login success');
+}
+
+})
+
+
+
+
+// login page end
